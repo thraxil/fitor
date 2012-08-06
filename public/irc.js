67fdd6ad06a8c8dba0eb3cc85a039b8b11011730
Anders Pearson <anders@columbia.edu>
2012-08-06 11:21:32 -0400

    handle server disconnects with a circuit breaker

--- public/irc.js
+++ public/irc.js
@@ -2,22 +2,32 @@ $(function() {
 	  var conn;
 	  var log = $("#log");
 
-	  function appendLog(msg) {
-        var d = log[0]
-        var doScroll = d.scrollTop == d.scrollHeight - d.clientHeight;
-        msg.appendTo(log)
-        if (doScroll) {
-            d.scrollTop = d.scrollHeight - d.clientHeight;
+    var currentRefresh = 1000;
+    var defaultRefresh = 1000;
+    var maxRefresh = 1000 * 5 * 60; // 5 minutes
+
+    var requestFailed = function(evt) {
+        // circuit breaker pattern for failed requests
+        // to ease up on the server when it's having trouble
+        currentRefresh = 2 * currentRefresh; // double the refresh time
+        if (currentRefresh > maxRefresh) {
+            currentRefresh = maxRefresh;
         }
-	  }
+        appendLog($("<div><b>Connection closed. trying again in " + currentRefresh/1000 + " seconds</b></div>"));
+        setTimeout(connectSocket,currentRefresh);
+    };
 
-	  if (window["WebSocket"]) {
+    var connectSocket = function() {
     	  conn = new WebSocket("ws://behemoth.ccnmtl.columbia.edu:5050/socket/");
-	      conn.onclose = function(evt) {
-	          appendLog($("<div><b>Connection closed.</b></div>"))
-	      }
-	      conn.onmessage = function(evt) {
+	      conn.onclose = requestFailed;
+	      conn.onmessage = onMessage;
+        conn.onopen = function (evt) {
+            currentRefresh = defaultRefresh;
+            appendLog($("<div><b>Connected to server.</b></div>"));
+        };
+    };
 
+    var onMessage = function (evt) {
 					  var data = JSON.parse(evt.data);
 					  var entry = $("<div/>");
             entry.addClass("row");
@@ -32,7 +42,19 @@ $(function() {
 					  entry.append("<div class='span1 nick'>&lt;" + data.Nick + "&gt;</div>");
 					  entry.append("<div class='span10 ircmessage'>" + data.Content + "</div>");
 	          appendLog(entry);
-	      }
+    }
+
+	  function appendLog(msg) {
+        var d = log[0]
+        var doScroll = d.scrollTop == d.scrollHeight - d.clientHeight;
+        msg.appendTo(log)
+        if (doScroll) {
+            d.scrollTop = d.scrollHeight - d.clientHeight;
+        }
+	  }
+
+	  if (window["WebSocket"]) {
+        connectSocket();
 	  } else {
 	      appendLog($("<div><b>Your browser does not support WebSockets.</b></div>"))
 	  }
