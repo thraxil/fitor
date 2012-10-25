7698a9f938cabb0cb9187eea02bc02e6c0d3abd4
Anders Pearson <anders@columbia.edu>
2012-10-25 13:13:11 -0400

    route messages through windsock broker

--- fitor.go
+++ fitor.go
@@ -4,17 +4,19 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	zmq "github.com/alecthomas/gozmq"
 	irc "github.com/fluffle/goirc/client"
 )
 
-var PUB_KEY = "gobot"
+var PUB_KEY = "gobot.fitor"
+var SUB_KEY = "gobot"
 var NICK = "gobot"
 var CHANNEL = "#ccnmtl"
 
-var PUB_SOCKET = "tcp://*:5556"
-var SUB_SOCKET = "tcp://localhost:5557"
+var REQ_SOCKET = "tcp://localhost:5555"
+var SUB_SOCKET = "tcp://localhost:5556"
 
 type Message struct {
 	MessageType string `json:"message_type"`
@@ -22,11 +24,28 @@ type Message struct {
 	Content     string `json:"content"`
 }
 
+// how we route zmq messages around
+type envelope struct {
+	Address string `json:"address"`
+	Content string `json:"content"`
+}
+
+func startswith(s, prefix string) bool {
+	if len(s) < len(prefix) {
+		return false
+	}
+	return s[:len(prefix)] == prefix
+}
+
 func receiveZmqMessage(subsocket zmq.Socket, m *Message) error {
 	// using zmq multi-part messages which will arrive
 	// in pairs. the first of which we don't care about so we discard.
-	_, _ = subsocket.Recv(0)
+	address, _ := subsocket.Recv(0)
 	content, _ := subsocket.Recv(0)
+	if startswith(string(address), PUB_KEY) {
+		// it's one that we sent out, so ignore it
+		return errors.New("do not echo my own messages")
+	}
 	return json.Unmarshal([]byte(content), m)
 }
 
@@ -49,10 +68,16 @@ func zmqToIrcLoop(conn *irc.Conn, subsocket zmq.Socket) {
 	}
 }
 
-// send a message to the zmq PUB socket
-func sendMessage(pubsocket zmq.Socket, m Message) {
+// send a message to the zmq REQ socket
+func sendMessage(reqsocket zmq.Socket, m Message) {
+	var address = PUB_KEY + "." + m.Nick
 	b, _ := json.Marshal(m)
-	pubsocket.SendMultipart([][]byte{[]byte(PUB_KEY), b}, 0)
+	var content = b
+	env := envelope{address, string(content)}
+	e, _ := json.Marshal(env)
+	reqsocket.Send([]byte(e), 0)
+	// wait for a reply
+	reqsocket.Recv(0)
 }
 
 func statusMessage(content string) Message {
@@ -66,13 +91,13 @@ func statusMessage(content string) Message {
 func main() {
 	// prepare our zmq sockets
 	context, _ := zmq.NewContext()
-	pubsocket, _ := context.NewSocket(zmq.PUB)
+	reqsocket, _ := context.NewSocket(zmq.REQ)
 	subsocket, _ := context.NewSocket(zmq.SUB)
 	defer context.Close()
-	defer pubsocket.Close()
+	defer reqsocket.Close()
 	defer subsocket.Close()
-	pubsocket.Bind(PUB_SOCKET)
-	subsocket.SetSockOptString(zmq.SUBSCRIBE, PUB_KEY)
+	reqsocket.Connect(REQ_SOCKET)
+	subsocket.SetSockOptString(zmq.SUBSCRIBE, SUB_KEY)
 	subsocket.Connect(SUB_SOCKET)
 
 	// configure our IRC client
@@ -81,26 +106,28 @@ func main() {
 	// most of the functionality is arranged by adding handlers
 	c.AddHandler("connected", func(conn *irc.Conn, line *irc.Line) {
 		conn.Join(CHANNEL)
-		sendMessage(pubsocket, statusMessage("joined "+CHANNEL))
+		sendMessage(reqsocket, statusMessage("joined "+CHANNEL))
 		// spawn a goroutine that will do the ZMQ -> IRC bridge
 		go zmqToIrcLoop(conn, subsocket)
 	})
 
 	quit := make(chan bool)
 	c.AddHandler("disconnected", func(conn *irc.Conn, line *irc.Line) {
-		sendMessage(pubsocket, statusMessage("disconnected"))
+		sendMessage(reqsocket, statusMessage("disconnected"))
 		quit <- true
 	})
 	// this is the handler that gets triggered whenever someone posts
 	// in the channel
 	c.AddHandler("PRIVMSG", func(conn *irc.Conn, line *irc.Line) {
 		// forward messages from IRC -> zmq PUB socket
-		sendMessage(pubsocket, Message{"message", line.Nick, line.Args[1]})
+		if line.Nick != NICK {
+			sendMessage(reqsocket, Message{"message", line.Nick, line.Args[1]})
+		}
 	})
 
 	// Now we can connect
 	if err := c.Connect("irc.freenode.net"); err != nil {
-		sendMessage(pubsocket, statusMessage("error connecting: "+err.Error()))
+		sendMessage(reqsocket, statusMessage("error connecting: "+err.Error()))
 		fmt.Printf("Connection error: %s\n", err.Error())
 	}
 
